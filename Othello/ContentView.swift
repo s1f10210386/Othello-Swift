b91697ce2b6a7c0343dff29386ea60086a85e252
hotaka <s1f102103864@iniad.org>
2024-01-07 05:51:49 +0900

    Initial Commit

---
+++ Othello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Othello
+//
+//  Created by 金澤帆高 on 2024/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Othello/OthelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  OthelloApp.swift
+//  Othello
+//
+//  Created by 金澤帆高 on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct OthelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
